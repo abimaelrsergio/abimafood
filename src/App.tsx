9d48086081d5f2518652a2e8631a011e4b09c2ea
Abimael Sergio <abimaelsergio@rivian.com>
2023-09-13 19:19:08 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
-import FormularioRestaurantes from './paginas/Administracao/Restaurantes/FormularioRestaurantes';
+import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 
@@ -11,7 +11,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
       <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
-      <Route path="/admin/restaurantes/novo" element={<FormularioRestaurantes />} />
+      <Route path="/admin/restaurantes/novo" element={<FormularioRestaurante />} />
+      <Route path="/admin/restaurantes/:id" element={<FormularioRestaurante />} />
     </Routes>
   );
 }
